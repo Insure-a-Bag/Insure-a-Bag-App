648c68ac895ff5f741fdeac2c364d1e17593e255
Shanmugadevan <shanmugadevan@protonmail.com>
2023-03-11 16:21:35 +0530

    Added rainbowkit, wagmi and ethers dependencies

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+
+export default function Navbar() {
+    
+    return <ConnectButton/>
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,41 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import "@rainbow-me/rainbowkit/styles.css"
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { configureChains, createClient, WagmiConfig } from "wagmi"
+import {
+	polygonMumbai,
+	baseGoerli,
+	optimismGoerli,
+	goerli,
+	arbitrumGoerli,
+} from "wagmi/chains"
+import { alchemyProvider } from "wagmi/providers/alchemy"
+import { publicProvider } from "wagmi/providers/public"
+
+const { chains, provider } = configureChains(
+	[polygonMumbai, optimismGoerli, goerli],
+	[
+		// alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+		publicProvider(),
+	],
+)
+const { connectors } = getDefaultWallets({
+	appName: "Insure-A-Bag",
+	chains,
+})
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains}>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	)
 }
